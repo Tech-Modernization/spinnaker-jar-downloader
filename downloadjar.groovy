8a1c689eca5a0249a6555486ddc3fff9d134aea2
Matt Kuritz <mkuritz@gmail.com>
2017-09-15 16:50:39 -0400

    implement cf deploy descr class

--- downloadjar.groovy
+++ downloadjar.groovy
@@ -2,6 +2,26 @@
 
 import groovy.util.CliBuilder
 
+// Dummy class to minimally immitate spinnaker implementation
+class CloudFoundryDeployDescription {
+
+  String serverGroupName
+  String repository
+  String artifact
+  String username
+  String password
+
+  CloudFoundryDeployDescription(options) {
+    if (options.'s') {
+      this.serverGroupName = options.'s'
+    }
+    this.repository = options.'r'
+    this.artifact = options.'a'
+    this.username = options.'u'
+    this.password = options.'p'
+  }
+}
+
 def returnOpts(args) {
     def cli = new CliBuilder(usage: './downloadjar.groovy -[hraups]')
     // Create the list of options.
@@ -33,10 +53,14 @@ def returnOpts(args) {
 }
 
 def downloadjar(args) {
-  new options = returnOpts(args)
+  def options = returnOpts(args)
+  def description = new CloudFoundryDeployDescription(options)
 
   for (option in ['a', 'r', 'u', 'p']) {
     println "${option}: ${options[option]}"
+  }
+  for (option in ['artifact', 'repository', 'username', 'password']) {
+    println "${option}: ${description[option]}"
   }
 }
 
